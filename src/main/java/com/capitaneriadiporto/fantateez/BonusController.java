73ae8b3f8555e9bc3845d1b2cc37d05f8b11b19a
veronica <vlivenoti@gmail.com>
2023-10-05 19:16:42 +0200

    popup conferma reset teams(ADMIN)

--- src/main/java/com/capitaneriadiporto/fantateez/BonusController.java
+++ src/main/java/com/capitaneriadiporto/fantateez/BonusController.java
@@ -8,6 +8,7 @@ import com.capitaneriadiporto.fantateez.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -71,7 +72,7 @@ public class BonusController {
         return "adminHomepage";
     }
 
-    @PostMapping("/resetTeams")
+    @GetMapping("/resetTeams")
     public String resetTeams(Model model){
         teamRepository.deleteAll();
         memberRepository.setScoresToZero();
